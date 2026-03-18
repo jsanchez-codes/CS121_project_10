f3fc30a3d273f96a455ed48579c5c584c68ea1b9
jsanchez-codes <julian.sanchez@bsu.edu>
2026-03-18 19:19:57 +0000

    Created CheckingAccount

---
+++ CheckingAccount.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class CheckingAccount implements HasMenu, Serializable {
+	double balance;
+
+	public static void main(String[] args){
+		CheckingAccount ca = new CheckingAccount();
+		ca.start();
+	} // end main
+
+	public CheckingAccount(){
+		this.balance = 0d;
+	} // end constructor
+	
+	public CheckingAccount(double balance){
+		this.balance = balance;
+	} // end constructor
+
+	public String menu(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("0) Quit ");
+		System.out.println("1) Check Balance");
+		System.out.println("2) Make a Deposit");
+		System.out.println("3) Make a Withdrawal");
+		System.out.println();
+		System.out.print("Please enter 0-3: ");
+		String response = input.nextLine();
+		return response;
+	} // end menu
+	
+	public void start(){
+		boolean keepGoing = false;
+		String response;
+		while(keepGoing){
+			response = menu();
+			if (response.equals("0")){
+				keepGoing = false;
+			} else if (response.equals("1")){
+				System.out.println(" Checking Balance... ");
+			} else if (response.equals("2")){
+				System.out.println(" Making a Deposit... ");
+			} else if (response.equals("3")){
+				System.out.println(" Making a Withdrawal... ");
+			} else {
+				System.out.println(" Please enter 0, 1, 2, or 3 ");
+			} // end if
+		} // end while
+	} // end start
+	
+} // end class def
+
