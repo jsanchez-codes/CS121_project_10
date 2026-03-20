b69bee549045728951bd1b157ccae08dfdecaf14
jsanchez-codes <julian.sanchez@bsu.edu>
2026-03-20 21:34:49 +0000

    Updated User

--- User.java
+++ User.java
@@ -49,7 +49,7 @@ public abstract class User implements Serializable, HasMenu {
 			String pinIn = input.nextLine();
 
 			if (pinIn.equals(this.PIN)){
-				System.out.println("Login succesful");
+				System.out.println("Login successful");
 				result = true;
 			} else {
 				System.out.println("Incorrect PIN");
