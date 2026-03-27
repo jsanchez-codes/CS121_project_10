cd9df95b14a8c5b7c2edf3cea561369e22d58d51
jsanchez-codes <julian.sanchez@bsu.edu>
2026-03-27 00:12:50 +0000

    Black Belt Version

--- Bank.java
+++ Bank.java
@@ -149,9 +149,15 @@ public class Bank implements HasMenu {
 		} // end try
 	} // end loadCustomers
 	
+	public void deleteUser(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("List of current customers: " + customers);
+		for (int i = 0; i < customers.size(); i++){
+			Customer deleteCustomer = customers.remove(i);
+			System.out.println("Deleted Customer: " + deleteCustomer);
+		} // end for
+	} // end deleteUser
 	
-	//public void deleteUser(){
-		//for (int i = 0; i < customers.size(); i++){
 } // end Bank
 
 class CustomerList extends ArrayList<Customer> {};
